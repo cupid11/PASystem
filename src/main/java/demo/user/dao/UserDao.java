40e99a35746492f3322c1734819fe7be40bf63e8
LeiJian <949426374@qq.com>
2018-07-23 15:26:10 +0800

    update

--- src/main/java/demo/user/dao/UserDao.java
+++ src/main/java/demo/user/dao/UserDao.java
@@ -1,12 +1,6 @@
 package demo.user.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import demo.basic.dao.BasicDao;
-import demo.repository.UserRepository;
-import demo.user.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 
